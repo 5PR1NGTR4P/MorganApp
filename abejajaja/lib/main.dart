881d99920d8606ea1853dbf3f71c4acbc0ab831f
CatNap <pollito_tony@outlook.com>
2025-11-20 20:13:50 -0600

    Se corrige el nombre de la app

--- abejajaja/lib/main.dart
+++ abejajaja/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Manahuia Apícola',
+      title: 'Abejajaja',
       theme: ThemeData(
         primarySwatch: Colors.amber,
         scaffoldBackgroundColor: Colors.white,
